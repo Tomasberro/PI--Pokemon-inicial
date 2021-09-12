a342b392bb6131814a069a99f143856036ab1323
Tomasberro <tomasberrotaran@gmail.com>
2021-09-12 18:52:48 -0300

    testing pi

--- api/tests/models/pokemon.spec.js
+++ api/tests/models/pokemon.spec.js
@@ -17,6 +17,24 @@ describe('Pokemon model', () => {
       it('should work when its a valid name', () => {
         Pokemon.create({ name: 'Pikachu' });
       });
+      it('should work when its a valid hp', () => {
+        Pokemon.create({ hp: 40 });
+      });
+      it('should work when its a valid attack', () => {
+        Pokemon.create({ attack: 38 });
+      });
+      it('should work when its a valid defense', () => {
+        Pokemon.create({ defense: 35 });
+      });
+      it('should work when its a valid speed', () => {
+        Pokemon.create({ speed: 30 });
+      });
+      it('should work when its a valid height', () => {
+        Pokemon.create({ height: 8 });
+      });
+      it('should work when its a valid weight', () => {
+        Pokemon.create({ weight: 120 });
+      });
     });
   });
 });
--- api/tests/routes/pokemon.spec.js
+++ api/tests/routes/pokemon.spec.js
@@ -3,8 +3,9 @@ const { expect } = require('chai');
 const session = require('supertest-session');
 const app = require('../../src/app.js');
 const { Pokemon, conn } = require('../../src/db.js');
-
+const rutaback= require('../../src/routes/pokemons')
 const agent = session(app);
+const agent2 = session(rutaback)
 const pokemon = {
   name: 'Pikachu',
 };
@@ -21,4 +22,28 @@ describe('Pokemon routes', () => {
       agent.get('/pokemons').expect(200)
     );
   });
+})
+  const pokemon2 = {
+    name: 'Tomas',
+    hp: 40
+  };
+  
+  describe('Pokemon routesback', () => {
+    before(() => conn.authenticate()
+    .catch((err) => {
+      console.error('Unable to connect to the database:', err);
+    }));
+    beforeEach(() => Pokemon.sync({ force: true })
+    .then(() => Pokemon.create(pokemon2)));
+    describe('POST /pokemons', () => {
+  it('POST agregar un nuevo pokemon y devuelve el nombre y hp del pokemon agregado', function() {
+     agent2.post('/pokemons')
+      .send(pokemon2)
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .expect(function(res) {
+        expect(res.body).toEqual("Tomas", 40);
+      });
+  });
 });
+}); 
